969b2dd9baef47612dfc61eda2008eb6eaa80b6d
techtigers33958 <53841748+techtigers33958@users.noreply.github.com>
2020-03-01 10:13:29 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,32 @@
+function setCharacter (char: string) {
+    string = char
+    basic.showString(char)
+}
+input.onGesture(Gesture.TiltRight, function () {
+    setCharacter("D")
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    setCharacter("C")
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    setCharacter("A")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString(string)
+})
+input.onButtonPressed(Button.B, function () {
+    string = ""
+    basic.showIcon(IconNames.Diamond)
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    setCharacter("B")
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendString(string)
+})
+let string = ""
+radio.setGroup(20)
+radio.setGroup(1)
+string = ""
+basic.showIcon(IconNames.Diamond)
+radio.sendString("START")
