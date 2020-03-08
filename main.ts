5b0a2de7bd31bdb8ba00c1a3216def94757947dd
techtigers33958 <53841748+techtigers33958@users.noreply.github.com>
2020-03-08 11:11:59 -0400

    Finished Commenting and Testing

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+/**
+ * Based on the movement, record a letter but do NOT send it.
+ */
 input.onGesture(Gesture.TiltRight, function () {
     setCharacter("D")
 })
@@ -7,13 +10,16 @@ input.onGesture(Gesture.TiltLeft, function () {
 input.onGesture(Gesture.ScreenUp, function () {
     setCharacter("A")
 })
+// When A+B are pressed, show the string
 input.onButtonPressed(Button.AB, function () {
     basic.showString(string)
 })
+// When B is pressed reset the string
 input.onButtonPressed(Button.B, function () {
     string = ""
     basic.showIcon(IconNames.Diamond)
 })
+// This function sets the string to char and shows it
 function setCharacter (char: string) {
     string = char
     basic.showString(char)
@@ -21,6 +27,7 @@ function setCharacter (char: string) {
 input.onGesture(Gesture.ScreenDown, function () {
     setCharacter("B")
 })
+// When A is pressed, send the string
 input.onButtonPressed(Button.A, function () {
     radio.sendString(string)
 })
